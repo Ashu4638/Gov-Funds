9c69712adf6c60ccc6efbbb90473812a5b903e71
Ashu <ashutoshkoli24@gmail.com>
2021-12-05 21:37:15 +0530

    Modified

--- Chain/urls.py
+++ Chain/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+
 from Chain import  views
 
 urlpatterns = [
@@ -34,5 +38,6 @@ urlpatterns = [
     path('viewtransaction', views.viewtransaction, name="viewtransaction"),
     path('addfund', views.addfund, name="addfund"),
     path('appendfund', views.appendfund, name="addfund"),
-
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
