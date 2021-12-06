7e7701881de66ab026c4cd6ba4c7f1fc83e5c3fa
Ashu <ashutoshkoli24@gmail.com>
2021-12-06 15:39:54 +0530

    Request Funds Added

--- Chain/views.py
+++ Chain/views.py
@@ -24,57 +24,74 @@ def index(request):
     return render(request, 'index.html', data)
 
 def admin(request):
-    return render(request,'admin.html' ,data)
+    if request.user.is_authenticated and request.user.username == "admin":
+        return render(request,'admin.html' ,data)
+    else:
+        return render(request, 'login.html')
+
 
 def loginform(request):
     if request.user.is_authenticated and request.user.username != "admin":
-        return render(request, 'transaction.html')
+        return render(request, 'transaction.html', data)
     elif request.user.is_authenticated and request.user.username == "admin":
         return render(request,'admin.html' ,data)
     else:
         return render(request, 'login.html')
 
 def createTransaction(request):
-    if request.method == 'POST':
-        fromAdd = request.POST['fromAdd']
-        toAdd = request.POST['toAdd']
-        amount = request.POST['amount']
-        data["funds"][0].createTransaction(transaction.Transaction(fromAdd, toAdd, amount))
-        print(data["funds"][0].pendingtransactions)
-        return render(request,'transaction.html')
+    if request.user.is_authenticated and request.user.username != "admin":
+        if request.method == 'POST':
+            fromAdd = request.POST.get('fundname')
+            toAdd = request.user.username
+            amount = int(request.POST['amount'])
+            print(fromAdd)
+            print(toAdd)
+            fund = data["funds"][0]
+            for fund in data["funds"]:
+                if fund.name == fromAdd:
+                    fund.createTransaction(transaction.Transaction(fromAdd, toAdd, amount))
+            return render(request,'transaction.html', data)
+        else:
+            return HttpResponse("404 - Not Allowed")
     else:
-        return HttpResponse("404 - Not Allowed")
+        return render(request, 'login.html')
 
 
 def Mine(request):
-    chain = data["funds"][0]
-    name = ""
-    if request.method == 'POST':
-        name = request.POST.get('block')
-        print(name)
-        for fund in data["funds"]:
-            print(fund)
-            if fund.name == name:
-                chain = fund
-                break
-        print(chain.pendingtransactions)
-    if len(chain.pendingtransactions) == 0:
+    if request.user.is_authenticated and request.user.username == "admin":
+        chain = data["funds"][0]
+        name = ""
+        if request.method == 'POST':
+            name = request.POST.get('block')
+            print(name)
+            for fund in data["funds"]:
+                print(fund)
+                if fund.name == name:
+                    chain = fund
+                    break
+            print(chain.pendingtransactions)
+        if len(chain.pendingtransactions) == 0:
 
-        messages.error(request, "No Transactions")
-        return render(request, 'admin.html', data)
+            messages.error(request, "No Transactions")
+            return render(request, 'admin.html', data)
+        else:
+            return render(request, 'Mine.html', {"transactions":chain.pendingtransactions, "name" : name})
     else:
-        return render(request, 'Mine.html', {"transactions":chain.pendingtransactions, "name" : name})
+        return render(request, 'login.html')
 
 def addblock(request):
-    if request.method == 'POST':
-        name = request.POST.get('block')
-        print("print")
-        print(name)
-        fund = data["funds"][0]
-        for fund in data["funds"]:
-            if fund.name == name:
-                fund.minependingTransacrions()
-        return render(request, 'blockchain.html', {"funds" : fund.chain} )
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            name = request.POST.get('block')
+            print("print")
+            print(name)
+            fund = data["funds"][0]
+            for fund in data["funds"]:
+                if fund.name == name:
+                    fund.minependingTransacrions()
+            return render(request, 'blockchain.html', {"funds" : fund.chain} )
+    else:
+        return render(request, 'login.html')
 def viewblockchain(request):
     if request.method == 'POST':
         name = request.POST.get('block')
@@ -99,17 +116,25 @@ def viewtransaction(request):
         return render(request, 'viewtransaction.html', {"transactions": block.transactions})
 
 def addfund(request):
-    return render(request, 'addFund.html')
+    if request.user.is_authenticated and request.user.username == "admin":
+        return render(request, 'addFund.html')
+    else:
+        return render(request, 'login.html')
+
 def appendfund(request):
-    if request.method == 'POST':
-        fundname = request.POST['fundname']
-        amount = request.POST['amount']
-        print(amount)
-        print(fundname)
-        temp = blockchain.Blockchain(int(amount), fundname)
-        print(temp.chain)
-        data["funds"].append(temp)
-    return render(request, 'addFund.html')
+    if request.user.is_authenticated and request.user.username == "admin":
+
+        if request.method == 'POST':
+            fundname = request.POST['fundname']
+            amount = request.POST['amount']
+            print(amount)
+            print(fundname)
+            temp = blockchain.Blockchain(int(amount), fundname)
+            print(temp.chain)
+            data["funds"].append(temp)
+        return render(request, 'addFund.html')
+    else:
+        return render(request, 'login.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -127,7 +152,7 @@ def handlelogin(request):
             else:
                 login(request, user)
                 messages.success(request, "Login Sucess")
-                return render(request,'transaction.html')
+                return render(request,'transaction.html', data)
     else:
         return HttpResponse("404 - Not Allowed")
     return HttpResponse("None")
